ada1458285c879b15b7254835c33856985147367
KATTACHANDU <129422555+KATTACHANDU@users.noreply.github.com>
2023-06-10 02:58:59 +0530

    Add files via upload

---
+++ chandu/chandu/src/main/java/com/example/chandu/ChanduApplication.java
@@ -0,0 +1,15 @@
+package com.example.chandu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanduApplication.class, args);
+        System.out.println("chandu");
+
+    }
+
+}
+
